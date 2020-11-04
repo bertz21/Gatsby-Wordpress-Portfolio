397b4489a07fd7cfa48ecd4da27dda45b3e5c9d9
Helbert Nyoliman {{ 楊毅杰 }} <bertz21@users.noreply.github.com>
2020-11-04 13:21:19 -0700

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby WordPress Starter`,
@@ -32,8 +36,8 @@ module.exports = {
       resolve: "gatsby-source-wordpress",
       options: {
         minimizeDeprecationNotice: true,
-        baseUrl: "gatsby-wordpress.local",
-        protocol: "http",
+        baseUrl: process.env.API_URL,
+        protocol: process.env.API_PROTOCOL,
         hostingWPCOM: false,
         useACF: false,
         verboseOutput: false,
