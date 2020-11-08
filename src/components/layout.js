9d98210452a1ea527175a64d3cbfd1b4cd917594
Helbert Nyoliman {{ 楊毅杰 }} <bertz21@users.noreply.github.com>
2020-11-08 14:59:48 -0700

    Add files via upload

---
+++ src/components/layout.js
@@ -0,0 +1,38 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import MainMenu from './MainMenu'
+
+import styled, {createGlobalStyle} from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+
+  body{
+    font-family: 'Open Sans', sans-serif;
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+`
+const LayoutWrapper = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+`
+
+const Layout = ({ children }) => (
+  <div>
+    <GlobalStyles />
+    <MainMenu />
+    <LayoutWrapper>
+      {children}
+    </LayoutWrapper>
+    
+  </div>
+)
+
+export default Layout
